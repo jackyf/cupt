db327e4080774410f1e18fb2810a388429ce388a
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2009-01-14 01:12:48 +0200

    WIP: 'Cupt::SystemState'.

--- Cupt/SystemState.pm
+++ Cupt/SystemState.pm
@@ -51,23 +51,35 @@ sub _parse_dpkg_status {
 
 	eval {
 		while (<OFFSETS>) {
-			if (m/^(\d+):Package: (.*)/) { # '$' implied in regexp
-				my $package_name = $2;
+			# firstly, make sure that this is 'Package' line
+			# "12345-" is prefix by grep
+			m/^(\d+)-Package: (.*)/) or
+					mydie("expected 'Package' line, but haven't got it");
 
-				# offset is returned by grep -b, and we skips 'Package: <...>' line additionally
-				my $offset = $1 + length("Package: $package_name\n");
+			# don't check package name for correctness, dpkg has to check this already
+			my $package_name = $2;
 
-				# don't check it for correctness, dpkg has to be done this already
+			# now make sure that next line is proper 'Status' line
+			m/^(\d+):Status (.*)/ or
+					mydie("expected 'Status' line, but haven't got it (for package '%s')", $package_name);
 
-				# adding new version to cache
-				$self->{cache}->{binary_packages}->{$package_name} //= Cupt::Cache::Pkg->new();
+			my %installed_info;
+			($installed_info{'want'}, $installed_info{'flag'}, $installed_info{'status'}) =
+					split / /, $2 or
+					mydie("malformed 'Status' line (for package '%s')", $package_name);
 
-				Cupt::Cache::Pkg::add_entry(
-						$self->{cache}->{binary_packages}->{$package_name}, 'Cupt::Cache::BinaryVersion',
-						$package_name, $fh, $offset, undef, \%Cupt::Cache::_empty_release_info);
-			} else {
-				mydie("expected 'Package' line, but haven't got it");
-			}
+			
+
+			# offset is returned by grep -b, and we skips 'Package: <...>' line additionally
+			my $offset = $1 + length("Package: $package_name\n");
+
+
+			# adding new version to cache
+			$self->{cache}->{binary_packages}->{$package_name} //= Cupt::Cache::Pkg->new();
+
+			Cupt::Cache::Pkg::add_entry(
+					$self->{cache}->{binary_packages}->{$package_name}, 'Cupt::Cache::BinaryVersion',
+					$package_name, $fh, $offset, undef, \%Cupt::Cache::_empty_release_info);
 		}
 	};
 	if (mycatch()) {
