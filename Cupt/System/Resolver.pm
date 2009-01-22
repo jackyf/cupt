8a3ebba3473cd6c0a58b7031c60b549eb9a9b3c8
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2009-01-22 15:38:22 +0200

    WIP: system resolver.

--- Cupt/System/Resolver.pm
+++ Cupt/System/Resolver.pm
@@ -5,8 +5,6 @@ use warnings;
 
 use Cupt::Core;
 
-use fields qw(config params packages pending_relations);
-
 =head1 FIELDS
 
 =head2 config
@@ -39,20 +37,7 @@ packages, or for satisfying some requested relations
 
 =cut
 
-sub new {
-	my $class = shift;
-	my $self = fields::new($class);
-
-	# common apt config
-	$self->{config} = shift;
-
-	$self->{cache} = shift;
-
-	# resolver params
-	$self->{params} = (
-		@_;
-	);
-}
+use fields qw(config params packages pending_relations);
 
 =head1 METHODS
 
@@ -68,13 +53,19 @@ I<cache> - reference to Cupt::Cache
 
 =cut
 
-sub import_versions ($$) {
-	my ($self, $ref_versions) = @_;
+sub new {
+	my $class = shift;
+	my $self = fields::new($class);
 
-	foreach my $version = (@$ref_versions) {
-		# just moving versions to packages, don't try install or remove some dependencies
-		$self->{packages}->{$version->{package_name}}->{version} = $version;
-	}
+	# common apt config
+	$self->{config} = shift;
+
+	$self->{cache} = shift;
+
+	# resolver params
+	$self->{params} = (
+		@_;
+	);
 }
 
 =head2 import_versions
@@ -88,9 +79,17 @@ I<ref_versions> - reference to array of Cupt::Cache::BinaryVersion
 
 =cut
 
-sub install_version ($$) {
-	my ($self, $version} = @_;
-	$self->{packages}->{$version->{package_name}}->{version} = $version;
+sub import_versions ($$) {
+	my ($self, $ref_versions) = @_;
+
+	foreach my $version = (@$ref_versions) {
+		# just moving versions to packages, don't try install or remove some dependencies
+		$self->{packages}->{$version->{package_name}}->{version} = $version;
+	}
+}
+
+sub _schedule_new_version_relations ($$) {
+	my ($self, $version);
 
 	if (defined($version->{depends})) {
 		# ok, unconditionally adding depends
@@ -112,6 +111,23 @@ sub install_version ($$) {
 	}
 }
 
+=head2 install_version
+
+member function, installs a new version with requested depends
+
+Parameters:
+
+I<version> - reference to Cupt::Cache::BinaryVersion
+
+=cut
+
+sub install_version ($$) {
+	my ($self, $version} = @_;
+	$self->{packages}->{$version->{package_name}}->{version} = $version;
+
+	$self->_schedule_new_version_relations($version);
+}
+
 =head2 satisfy_relation
 
 member function, installs all needed versions to satisfy relation or relation group
@@ -129,21 +145,6 @@ sub satisfy_relation ($$) {
 	push @{$self{pending_relations}}, $relation_expression;
 }
 
-=head2 install_version
-
-member function, installs a new version with requested depends
-
-Parameters:
-
-I<version> - reference to Cupt::Cache::BinaryVersion
-
-=cut
-
-sub remove_package ($$) {
-	my ($self, $package_name) = @_;
-	delete $self->{packages}->{$package_name};
-}
-
 =head2 remove_package
 
 member function, removes a package
@@ -154,15 +155,39 @@ I<package_name> - string, name of package to remove
 
 =cut
 
+sub remove_package ($$) {
+	my ($self, $package_name) = @_;
+	delete $self->{packages}->{$package_name};
+}
+
 # every package version has a weight
-sub __version_weight {
-	my ($version) = @_;
+sub __version_weight ($$) {
+	my ($self, $version) = @_;
 	my $result = $self->{cache}->get_pin($version);
 	$result += 5000 if $version->{essential} eq 'yes';
 	$result += 2000 if $version->{priority} eq 'required';
 	$result += 1000 if $version->{priority} eq 'important';
 	$result += 400 if $version->{priority} eq 'standard';
 	$result += 100 if $version->{priority} eq 'optional';
+}
+
+sub _recursive_resolve ($$) {
+	my ($self, $sub_accept) = @_;
+
+	while (my $package_name = each $self->{packages}) {
+		my $package_entry = $self->{packages}->{$package_name};
+		my $version = $package_entry->{version};
+
+		# checking that all 'Depends' are satisfied
+		if (defined($version->{depends})) {
+			foreach (@$version->{depends} {
+				my $ref_satisfying_versions = $self->{cache}->get_satisfying_relations($_);
+				if (scalar @$ref_satisfying_versions == 0) {
+					# oh, bad, no versions can satisfy this package
+					if (exists $package_entry->{stick}) {
+						# and we can't even remove it... fail then
+						return 0;
+	}
 }
 
 =head2 resolve
@@ -181,12 +206,23 @@ true if some solution was found and accepted, false otherwise
 =cut
 
 sub resolve ($$) {
-	my ($self, $sub_accept) = 0;
+	my ($self, $sub_accept) = @_;
 
-	while (my $package_name = each $self->{packages}) {
-		my $package_entry = $self->{packages}->{$package_name};
-		my $version = $package_entry->{version};
+	# unwinding relations
+	while (scalar $self->{pending_relations}) {
+		my $relation_expression = shift $self->{pending_relations};
+		my $ref_satisfying_versions = $self->{cache}->get_satisfying_relations($relation_expression);
+		
+		# if we have no candidates, skip the relation
+		scalar @$ref_satisfying_versions or next;
+
+		# installing most preferrable version
+
+		$self->install_version($ref_satisfying_versions->[0]);
+		# note that install_version can add some pending relations
 	}
+
+	# at this stage we have all extraneous dependencies installed, now we should check inter-depends
 }
 
 1;
