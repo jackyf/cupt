c60b5d90f5b2616fab4e2a771e104a2699ea02bb
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2009-03-07 19:37:46 +0200

    download manager: first import

---
+++ Cupt/Download/Manager.pm
@@ -0,0 +1,27 @@
+package Cupt::Download::Manager
+
+use 5.10.0;
+use strict;
+use warnings;
+
+use fields qw(_curl_share_handle);
+
+use Cupt::Core;
+
+use WWW::Curl::Share;
+
+=head1 new
+
+creates new Cupt::Download::Manager and returns reference to it
+
+=cut
+
+sub new {
+	my $class = shift;
+	my $self = fields::new($class);
+
+	$self->{_curl_share_handle} = new WWW::Curl::Share;
+}
+
+=head2 
+
