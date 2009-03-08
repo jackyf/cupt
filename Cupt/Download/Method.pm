133bad2f4ed61adfaecbe58a562650cc2c514ae1
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2009-03-08 17:31:40 +0200

    downloader: WIP

--- Cupt/Download/Method.pm
+++ Cupt/Download/Method.pm
@@ -2,23 +2,11 @@ package Cupt::Download::Method
 
 =head1 ABSTRACT
 
-Base class of all cupt downloading methods. It should be never instantiated
+Base package of all cupt downloading methods. It should be never instantiated
 directly. If you want to wrote your own download method for Cupt, you are need
-to implement all methods specified under.
+to implement 'pefrom' method specified under.
 
 =head1 METHODS
-
-=head2 new
-
-return the reference to Cupt::Download::Method.
-
-Parameters:
-
-I<config> - reference to Cupt::Config
-
-I<uri> - string that determines which URL to download
-
-I<filename> - target file name
 
 =cut
 
@@ -31,9 +19,25 @@ sub new {
 
 downloads specified file
 
+Parameters:
+
+I<config> - reference to Cupt::Config
+
+I<uri> - string that determines which URL to download
+
+I<filename> - target file name
+
+I<sub_callback> - subroutine to report status change of download, takes two arguments: I<name> and I<value>.
+Allowed pairs I<name> - I<value>:
+  "dns" - "<hostname>";
+  "connecting" - "<ip>";
+  "expected-size" - "<size of file to download>";
+  "downloading" - "<number of bytes done for download>" (in case of resumed
+download this number should include already downloaded size).
+
 =cut
 
-sub perform {
+sub perform ($$$$$) {
 	# stub
 }
 
--- Cupt/Download/Methods/Curl.pm
+++ Cupt/Download/Methods/Curl.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use 5.10.0;
 
-use fields qw(_config _curl_handle _filename);
+use base qw(Cupt::Download::Method);
 
 use Cupt::Core;
 use WWW::Curl::Easy 4.05;
@@ -17,35 +17,29 @@ $_curl_share_handle->setopt(CURLOPT_SHARE, CURL_LOCK_DATA_DNS);
 $_curl_share_handle->setopt(CURLOPT_SHARE, CURL_LOCK_DATA_CONNECT);
 $_curl_share_handle->setopt(CURLOPT_SHARE, CURL_LOCK_DATA_SSL_SESSION);
 
-sub new {
-	my ($class, $config, $uri, $filename) = shift;
-	my $self = fields::new($class);
-	$self->{_config} = $config;
-	$self->{_uri} = $uri;
-	$self->{_filename} = $filename;
-	return $self;
-}
-
-sub perform ($) {
-	my ($self)
+sub perform ($$$$$) {
+	my ($self, $config, $uri, $filename, $sub_callback) = shift;
 
 	my $curl = new WWW::Curl::Easy;
-	open(my $fd, '>>', $self->{_filename});
+	open(my $fd, '>>', $filename});
 
 	my $total_bytes = tell($fd);
 
 	my $sub_writefunction = sub {
+		# writing data to file
 		print { $_[1] } $_[0];
+
 		my $written_bytes = length($_[0]);
-		print "Written bytes: $written_bytes, total bytes: $total_bytes\n";
 		$total_bytes += $written_bytes;
+		$sub_callback->('downloading', $total_bytes);
+
 		return $written_bytes;
 	}
 
-	my ($protocol) = ($self->{_uri} =~ m{(\w+)::/});
+	my ($protocol) = ($uri =~ m{(\w+)::/});
 
-	$curl->setopt(CURLOPT_URL, $self->{_uri});
-	$curl->setopt(CURLOPT_MAX_RECV_SPEED_LARGE, $self->{_config}->var("acquire::${protocol}::dl-limit");
+	$curl->setopt(CURLOPT_URL, $uri);
+	$curl->setopt(CURLOPT_MAX_RECV_SPEED_LARGE, $config->var("acquire::${protocol}::dl-limit");
 	$curl->setopt(CURLOPT_WRITEFUNCTION, \&writefunction);
 	$curl->setopt(CURLOPT_WRITEDATA, $fd);
 	$curl->setopt(CURLOPT_RESUME_FROM, tell($fd));
