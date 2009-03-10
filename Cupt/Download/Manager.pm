0aa9def3a1c811bd44a6a529b36a898e4c263422
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2009-03-10 22:10:34 +0200

    manager: WIP

--- Cupt/Download/Manager.pm
+++ Cupt/Download/Manager.pm
@@ -3,6 +3,7 @@ package Cupt::Download::Manager
 use 5.10.0;
 use strict;
 use warnings;
+use threads;
 
 use fields qw(_active_queries _waiting_queries _downloads_done);
 
@@ -24,9 +25,10 @@ sub new {
 	$self->{_downloads_done} = {};
 }
 
-=head2 add
+=head2 download
 
-method, adds download query to queue
+method, adds download query to queue. Blocks execution of program until
+download is done.
 
 Parameters:
 
@@ -34,12 +36,53 @@ I<uri> - URI to download
 
 I<filename> - target filename
 
+Example:
+
+  my $download_manager = new Cupt::Download::Manager;
+  $download_manager->download('http://www.debian.org' => '/tmp/123.html');
+
 =cut
 
 sub download ($$$) : locked method {
 	my ($self, $uri, $filename) = @_;
 
-	if 
-	push $self->{_waiting_queries};
+	return $self->_download($uri, $filename);
 }
 
+=head2 download_group
+
+method, adds group of download queries to queue. Blocks execution of program until
+all downloads from groups are done.
+
+Parameters:
+
+Sequence of pairs of:
+
+I<uri> - URI to download
+
+I<filename> - target filename
+
+Example:
+
+  my $download_manager = new Cupt::Download::Manager;
+  $download_manager->download_group(
+      'http://www.en.debian.org' => '/tmp/en.html',
+	  'http://www.ru.debian.org' => '/tmp/ru.html',
+	  'http://www.ua.debian.org' => '/tmp/ua.html'
+  );
+
+=cut
+
+sub download_group ($$) : locked method {
+	my $self = shift;
+
+	my @threads;
+	# schedule download of each uri at its own thread
+	while (scalar @_) {
+		# extract next pair
+		my $uri = shift;
+		my $filename = shift;
+
+		threads->create(\&_download, $self, $uri, $filename);
+	}
+}
