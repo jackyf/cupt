jackyf/cupt

commit 198bb61060d7c4168e21f990f0f9db828de9a669

worker: don't fake 'remove' actions, just use existing 'install' ones instead

Cupt/System/Worker.pm
@@ -367,14 +367,14 @@ sub __is_inner_actions_equal ($$) {
 			$ref_left_action->{'action_name'} eq $ref_right_action->{'action_name'});
 }
 
-sub _fill_actions ($$\@) {
+sub _fill_actions ($\%$) {
 	my ($self, $ref_actions_preview, $graph) = @_;
 
 	# user action - action name from actions preview
 	my %user_action_to_inner_actions = (
 		'install' => [ 'install' ],
-		'upgrade' => [ 'remove', 'install' ],
-		'downgrade' => [ 'remove', 'install' ],
+		'upgrade' => [ 'install' ],
+		'downgrade' => [ 'install' ],
 		'configure' => [ 'configure' ],
 		'deconfigure' => [ 'remove' ],
 		'remove' => [ 'remove' ],
@@ -560,6 +560,11 @@ sub _build_actions_graph ($$) {
 				# depends must be removed after
 				$self->_fill_action_dependencies(
 						$system_version->{depends}, 'remove', 'after', $ref_inner_action, $graph);
+				# 'install' actions may also implicitly contain 'remove' for upgrades, so
+				$self->_fill_action_dependencies(
+						$system_version->{pre_depends}, 'install', 'after', $ref_inner_action, $graph);
+				$self->_fill_action_dependencies(
+						$system_version->{depends}, 'install', 'after', $ref_inner_action, $graph);
 			}
 		}
 	}

commit 125b6b2df556c0bf5e17396b1b729343696850fe

worker: _build_actions_graph: fix regression introduced in 198bb61060d7c4168e21f990f0f9db828de9a669: don't forget to also include remove-like dependencies for old versions of upgraded/downgraded packages

Cupt/System/Worker.pm
@@ -562,6 +562,29 @@ sub _build_actions_graph ($$) {
 						$system_version->{depends}, 'install', 'after', $ref_inner_action, $graph);
 			}
 		}
+
+		if ($ref_inner_action->{'action_name'} eq 'install') {
+			my $package = $self->{_cache}->get_binary_package($package_name);
+			if (defined $package) {
+				# this is an upgrade/downgrade
+
+				# schedule dependencies including the old version of the package
+				my $system_version = $package->get_installed_version();
+				# so perform just like 'remove'
+
+				# pre-depends must be removed after
+				$self->_fill_action_dependencies(
+						$system_version->{pre_depends}, 'remove', 'after', $ref_inner_action, $graph);
+				# depends must be removed after
+				$self->_fill_action_dependencies(
+						$system_version->{depends}, 'remove', 'after', $ref_inner_action, $graph);
+				# 'install' actions may also implicitly contain 'remove' for upgrades, so
+				$self->_fill_action_dependencies(
+						$system_version->{pre_depends}, 'install', 'after', $ref_inner_action, $graph);
+				$self->_fill_action_dependencies(
+						$system_version->{depends}, 'install', 'after', $ref_inner_action, $graph);
+			}
+		}
 	}
 
 	return $graph->strongly_connected_graph();