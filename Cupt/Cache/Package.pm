jackyf/cupt

commit 15e8086d28e644359ba19ce889d1a0316091c0d5

cache: allow reinstalling cupt: new subcommand 'reinstall'

Cupt/Cache/Package.pm
@@ -33,7 +33,8 @@ use Exporter qw(import);
 
 our @EXPORT = qw(&compare_versions);
 
-use Cupt::LValueFields qw(_unparsed_versions _parsed_versions _binary_architecture);
+use Cupt::LValueFields qw(_unparsed_versions _parsed_versions
+		_binary_architecture _allow_reinstall);
 
 use Cupt::Core;
 
@@ -59,10 +60,11 @@ returns a new Cupt::Cache::Package object. Usually shouldn't be called by hand.
 =cut
 
 sub new {
-	my ($class, $binary_architecture) = @_;
+	my ($class, $binary_architecture, $allow_reinstall) = @_;
 	my $self = bless [] => $class;
 	$self->[_unparsed_versions_offset()] = [];
 	$self->[_binary_architecture_offset()] = $binary_architecture;
+	$self->[_allow_reinstall_offset()] = $allow_reinstall;
 	return $self;
 }
 
@@ -100,6 +102,9 @@ sub get_versions {
 				eval {
 					my $version_class = shift @$ref_params;
 					$parsed_version = $version_class->new($ref_params);
+					if ($parsed_version->is_installed() and $self->[_allow_reinstall_offset()]) {
+						$parsed_version->version_string .= '~installed';
+					}
 					unshift @$ref_params, $version_class;
 				};
 				if (mycatch()) {

Cupt/Cache.pm
@@ -33,7 +33,7 @@ use warnings;
 use Digest;
 use Fcntl qw(:seek :DEFAULT);
 use POSIX qw(locale_h);
-use List::MoreUtils 0.23 qw(none);
+use List::MoreUtils 0.23 qw(none any);
 
 use Memoize;
 memoize('verify_signature');
@@ -62,7 +62,7 @@ I<get_satisfying_versions> subroutine for rapid lookup.
 
 use fields qw(_source_packages _binary_packages _config _pin_settings _system_state
 		_can_provide _extended_info _index_entries _release_data
-		_binary_architecture);
+		_binary_architecture _allow_reinstall);
 
 =head1 FLAGS
 
@@ -96,6 +96,8 @@ Next params are treated as hash-style param list:
 '-binary': read Packages
 
 '-installed': read dpkg status file
+
+'-allow-reinstall': list of globs of package names which are allowed to reinstall
 
 Example:
 
@@ -135,8 +137,14 @@ sub new {
 		'-source' => 1,
 		'-binary' => 1,
 		'-installed' => 1,
+		'-allow-reinstall' => [],
 		@_, # applying passed parameters
 	);
+
+	foreach my $allow_reinstall_glob (@{$build_config{'-allow-reinstall'}}) {
+		glob_to_regex(my $allow_reinstall_regex = $allow_reinstall_glob);
+		push @{$self->{_allow_reinstall}}, $allow_reinstall_regex;
+	}
 
 	if ($build_config{'-installed'}) {
 		# read system settings
@@ -417,7 +425,9 @@ sub _prepare_package {
 
 		# there are some version entries for this package, create it
 		my @unparsed_versions = @{$$ref_storage->{$package_name}};
-		$$ref_storage->{$package_name} = Cupt::Cache::Package->new($self->{_binary_architecture});
+		my $allow_reinstall = (any { $package_name =~ m/^$_$/ } @{$self->{_allow_reinstall}}) // 0;
+		$$ref_storage->{$package_name} =
+				Cupt::Cache::Package->new($self->{_binary_architecture}, $allow_reinstall);
 		$$ref_storage->{$package_name}->add_entry(@$_) for @unparsed_versions;
 	}
 }

cupt
@@ -120,6 +120,7 @@ sub main {
 		'screenshots' => \&screenshots,
 		'update' => \&update_release_data,
 		'install' => sub { manage_packages('install') },
+		'reinstall' => sub { manage_packages('reinstall') },
 		'remove' => sub { manage_packages('remove') },
 		'purge' => sub { manage_packages('purge') },
 		'satisfy' => sub { manage_packages('satisfy') },
@@ -389,6 +390,7 @@ sub help () {
 		'screenshots' => __('views Debian screenshot web pages for the binary package(s)'),
 		'update' => __('updates repository metadata'),
 		'install' => __('installs/upgrades/downgrades binary package(s)'),
+		'reinstall' => __('reinstalls binary packages(s)'),
 		'remove' => __('removes binary package(s)'),
 		'purge' => __('removes binary package(s) along with their configuration files'),
 		'satisfy' => __('performs actions to make relation expressions satisfied'),
@@ -1474,9 +1476,21 @@ sub manage_packages ($) {
 	check_no_more_options();
 
 	local $| = 1;
-	print __('Building the package cache... ');
-	build_cache(-source => ($action eq 'build-depends'), -binary => 1, -installed => 1);
-	say __('[done]');
+	do {
+		my @package_expressions_to_reinstall;
+		if ($action eq 'reinstall') {
+			$action = 'install';
+			@package_expressions_to_reinstall = @ARGV;
+		}
+		print __('Building the package cache... ');
+		build_cache(
+				'-source' => ($action eq 'build-depends'),
+				'-binary' => 1,
+				'-installed' => 1,
+				'-allow-reinstall' => \@package_expressions_to_reinstall
+		);
+		say __('[done]');
+	};
 
 	print __('Initializing package resolver and worker... ');
 	my $resolver;

doc/cupt.1.pod
@@ -351,6 +351,16 @@ then upgrades the whole system.
 
 Recommended over L</full-upgrade> for major upgrades.
 
+=item reinstall
+
+reinstalls specified binary packages
+
+This subcommand receives list of binary package names.
+
+Examples:
+
+C<cupt reinstall libc6>
+
 =item markauto
 
 marks specified packages as automatically installed

commit 8aba93eff6479cbf6df28b739db7513a655aab5d

package: get_versions: fixed a crash regression for source versions since 15e8086d28e644359ba19ce889d1a0316091c0d5

Cupt/Cache/Package.pm
@@ -102,7 +102,10 @@ sub get_versions {
 				eval {
 					my $version_class = shift @$ref_params;
 					$parsed_version = $version_class->new($ref_params);
-					if ($parsed_version->is_installed() and $self->[_allow_reinstall_offset()]) {
+					if (ref $parsed_version eq 'Cupt::Cache::BinaryVersion' and
+						$parsed_version->is_installed() and
+						$self->[_allow_reinstall_offset()])
+					{
 						$parsed_version->version_string .= '~installed';
 					}
 					unshift @$ref_params, $version_class;