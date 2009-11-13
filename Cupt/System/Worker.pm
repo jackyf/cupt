jackyf/cupt

commit 8769b8dd920da55c550a1382d432352453e603a7

worker: change_system: check that 'cupt::worker::archives-space-limit' is numeric

Cupt/System/Worker.pm
@@ -1513,6 +1513,9 @@ sub change_system ($$) {
 	my $debug = $self->_config->var('debug::worker');
 	my $download_only = $self->_config->var('cupt::worker::download-only');
 	my $archives_space_limit = $self->_config->var('cupt::worker::archives-space-limit');
+	if ($archives_space_limit !~ m/^\d+$/) {
+		mydie("the option 'cupt::worker::archives-space-limit' should be numeric-only");
+	}
 
 	my $ref_actions_preview = $self->get_actions_preview();
 

commit d50236b50011eb16333c61dde61f8f0c593e3334

worker: change_system: fixed a crash regression since 8769b8dd920da55c550a1382d432352453e603a7

Cupt/System/Worker.pm
@@ -1513,7 +1513,7 @@ sub change_system ($$) {
 	my $debug = $self->_config->var('debug::worker');
 	my $download_only = $self->_config->var('cupt::worker::download-only');
 	my $archives_space_limit = $self->_config->var('cupt::worker::archives-space-limit');
-	if ($archives_space_limit !~ m/^\d+$/) {
+	if (defined $archives_space_limit and $archives_space_limit !~ m/^\d+$/) {
 		mydie("the option 'cupt::worker::archives-space-limit' should be numeric-only");
 	}
 