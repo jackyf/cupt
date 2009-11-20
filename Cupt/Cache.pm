jackyf/cupt

commit 5aa189408aef229a31e285f53d8044825bfa6832

cache: explicitly specified Digest::* modules in 'use'

Cupt/Cache.pm
@@ -30,7 +30,8 @@ use 5.10.0;
 use strict;
 use warnings;
 
-use Digest;
+use Digest::MD5;
+use Digest::SHA;
 use Fcntl qw(:seek :DEFAULT);
 use POSIX qw(locale_h);
 use List::MoreUtils 0.23 qw(none any);

commit 4e966df7423cc21d7d26ebe2bb27a1e1d92952a7

cache: verify_hash_sums: fixed a crash regression since 5aa189408aef229a31e285f53d8044825bfa6832

Cupt/Cache.pm
@@ -30,6 +30,7 @@ use 5.10.0;
 use strict;
 use warnings;
 
+use Digest;
 use Digest::MD5;
 use Digest::SHA;
 use Fcntl qw(:seek :DEFAULT);