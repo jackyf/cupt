5565a03ed36193dcf79111617fec1bf7e06f1a2d
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2010-09-28 23:13:03 +0300

    lib: common: moved out structs 'PointerLess' and 'PointerEqual'

--- cpp/lib/include/cupt/common.hpp
+++ cpp/lib/include/cupt/common.hpp
@@ -116,31 +116,6 @@ string globToRegexString(const string&);
 
 string __(const char*);
 
-template < class T >
-struct PointerLess
-{
-	bool operator()(const shared_ptr< T >& left, const shared_ptr< T >& right) const
-	{
-		return *left < *right;
-	}
-	bool operator()(const T* left, const T* right) const
-	{
-		return *left < *right;
-	}
-};
-template < class T >
-struct PointerEqual: public std::binary_function< shared_ptr< T >, shared_ptr< T >, bool >
-{
-	bool operator()(const shared_ptr< T >& left, const shared_ptr< T >& right) const
-	{
-		return *left == *right;
-	}
-	bool operator()(const T* left, const T* right) const
-	{
-		return *left == *right;
-	}
-};
-
 void consumePackageName(string::const_iterator begin, string::const_iterator end,
 		string::const_iterator& resultEnd);
 bool checkPackageName(const string& packageName, bool throwOnError = true);
