32d24199fdb1bf25b0d2e7699e42c9f3c57bc63d
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2010-10-03 12:03:32 +0300

    doc: reference: lib: file: put: documented

--- cpp/lib/include/cupt/file.hpp
+++ cpp/lib/include/cupt/file.hpp
@@ -110,7 +110,16 @@ class File
 	 * @param block container for read data
 	 */
 	void getFile(string& block);
-	void put(const string&);
+	/// writes data
+	/**
+	 * @param data - data to write
+	 */
+	void put(const string& data);
+	/// writes data
+	/**
+	 * @param data - pointer to the data buffer
+	 * @param size - size of the buffer
+	 */
 	void put(const char* data, size_t size);
 
 	bool eof() const;
