12a3239cdf8e5a5b47a1f7b8931f6aaf4458219b
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2010-10-06 16:22:27 +0300

    precompiled: FORIT: don't recompute container.end() on each iteration

--- cpp/precompiled/precompiled.hpp
+++ cpp/precompiled/precompiled.hpp
@@ -1,6 +1,6 @@
 #include <boost/xpressive/xpressive_dynamic.hpp>
 
-#define FORIT(variableName, storage) for (auto variableName = (storage).begin(); variableName != (storage).end(); ++variableName)
+#define FORIT(variableName, storage) for (auto variableName = (storage).begin(), variableName##End = (storage).end(); variableName != variableName##End; ++variableName)
 
 #include <cupt/common.hpp>
 
