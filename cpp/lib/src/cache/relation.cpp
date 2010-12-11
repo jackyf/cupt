jackyf/cupt

commit a098275fa7bf0ace3cf29160969a2b995983b894

lib: cache: relation: RelationExpression: getHashString: don't add one of spaces

cpp/lib/src/cache/relation.cpp
@@ -362,8 +362,6 @@ string RelationExpression::getHashString() const
 			++bufSize;
 			buf[bufSize] = ('0' + relation.relationType);
 			++bufSize;
-			buf[bufSize] = ' ';
-			++bufSize;
 			strcpy(buf + bufSize, relation.versionString.c_str());
 			bufSize += versionStringSize;
 		}

commit a3e6c148e416715158fb94478342c8023027e58b

lib: cache: relation: RelationExpression: getHashString: fixed possible size-check regression since a098275fa7bf0ace3cf29160969a2b995983b894

cpp/lib/src/cache/relation.cpp
@@ -355,7 +355,7 @@ string RelationExpression::getHashString() const
 		if (relation.relationType != Relation::Types::None)
 		{
 			auto versionStringSize = relation.versionString.size();
-			if (p + versionStringSize + 3 >= bufferEnd)
+			if (p + versionStringSize + 2 >= bufferEnd)
 			{
 				return string();
 			}