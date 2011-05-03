cfc9b4593430859ad42958e50ed9cd10e155ffca
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2011-05-03 11:02:23 +0300

    lib: cache: relation: fix compilation without precompiled headers

--- cpp/lib/src/cache/relation.cpp
+++ cpp/lib/src/cache/relation.cpp
@@ -15,6 +15,7 @@
 *   Free Software Foundation, Inc.,                                       *
 *   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA               *
 **************************************************************************/
+#include <algorithm>
 
 #include <cupt/common.hpp>
 #include <cupt/cache/relation.hpp>
