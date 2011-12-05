c90042f14d4c7dfc1023aa24d9ab60cea5629e59
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2011-12-05 13:26:52 +0200

    WIP

---
+++ cpp/lib/src/internal/nativeresolver/autoremoval.hpp
@@ -0,0 +1,33 @@
+/**************************************************************************
+*   Copyright (C) 2011 by Eugene V. Lyubimkin                             *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License                  *
+*   (version 3 or above) as published by the Free Software Foundation.    *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU GPL                        *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA               *
+**************************************************************************/
+#ifndef CUPT_INTERNAL_NATIVERESOLVER_AUTOREMOVAL
+#define CUPT_INTERNAL_NATIVERESOLVER_AUTOREMOVAL
+
+#include <cupt/fwd.hpp>
+
+namespace cupt {
+namespace internal {
+
+class AutoRemoval
+{
+	AutoRemoval(const Config&);
+	bool isAllowed(const string& packageName) const;
+};
+
+#endif
+
