5f15c8a6ad33c1aa20df7b8ac1b03e0698e0b3f7
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2012-02-23 10:09:13 +0200

    downloadmethods: debdelta: use format2() instead of deprecated sf()

--- cpp/downloadmethods/debdelta.cpp
+++ cpp/downloadmethods/debdelta.cpp
@@ -46,12 +46,12 @@ class DebdeltaMethod: public download::Method
 		delete method;
 		if (!deltaDownloadResult.empty())
 		{
-			return sf(__("delta download failed: %s"), deltaDownloadResult.c_str());
+			return format2(__("delta download failed: %s"), deltaDownloadResult);
 		}
 
 		// invoking a deb patcher
-		auto patchCommand = sf("debpatch --accept-unsigned %s / %s >/dev/null",
-				deltaDownloadPath.c_str(), targetPath.c_str());
+		auto patchCommand = format2("debpatch --accept-unsigned %s / %s >/dev/null",
+				deltaDownloadPath, targetPath);
 		auto patchResult = ::system(patchCommand.c_str());
 
 		// remove delta anyway
@@ -62,7 +62,7 @@ class DebdeltaMethod: public download::Method
 
 		if (patchResult != 0)
 		{
-			return sf(__("debpatch returned error code %d"), patchResult);
+			return format2(__("debpatch returned error code %d"), patchResult);
 		}
 
 		// all went ok
