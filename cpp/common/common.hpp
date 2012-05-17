8c039a50aff74076499dc483fee79fd258465b59
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2012-05-17 13:02:25 +0300

    common: common: refactoring: removed PointerLess and PointerEqual as not used anymore

--- cpp/common/common.hpp
+++ cpp/common/common.hpp
@@ -2,32 +2,3 @@
 
 #include <cupt/common.hpp>
 
-namespace cupt {
-
-template < class T >
-struct PointerLess
-{
-	bool operator()(const shared_ptr< T >& left, const shared_ptr< T >& right) const
-	{
-		return *left < *right;
-	}
-	bool operator()(const T* left, const T* right) const
-	{
-		return *left < *right;
-	}
-};
-template < class T >
-struct PointerEqual
-{
-	bool operator()(const shared_ptr< T >& left, const shared_ptr< T >& right) const
-	{
-		return *left == *right;
-	}
-	bool operator()(const T* left, const T* right) const
-	{
-		return *left == *right;
-	}
-};
-
-}
-
