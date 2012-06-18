d204258104b22c3ac1cc3415fd989359411ef1f5
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2012-06-18 13:59:11 +0300

    lib: internal: nativeresolver: dependencygraph: FillHelper: getVertexPtr: makeVertex: refactoring: use '{}-style' initialization

--- cpp/lib/src/internal/nativeresolver/dependencygraph.cpp
+++ cpp/lib/src/internal/nativeresolver/dependencygraph.cpp
@@ -523,9 +523,8 @@ class DependencyGraph::FillHelper
 		};
 		auto makeVertex = [this, &packageName, &version]() -> const VersionVertex*
 		{
-			static const forward_list< const Element* > nullElementList;
 			auto relatedVertexPtrsIt = __package_name_to_vertex_ptrs.insert(
-					{ packageName, nullElementList }).first;
+					{ packageName, {} }).first;
 			auto vertexPtr(new VersionVertex(relatedVertexPtrsIt));
 			vertexPtr->version = version;
 			__dependency_graph.addVertex(vertexPtr);
