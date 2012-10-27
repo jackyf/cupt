e8b2bd0c3c0722dc1ba88d60aaf5fb025b8facce
Eugene V. Lyubimkin <jackyf.devel@gmail.com>
2012-10-27 16:52:15 +0300

    WIP

---
+++ cpp/lib/include/cupt/packageid.hpp
@@ -0,0 +1,56 @@
+/**************************************************************************
+*   Copyright (C) 2012 by Eugene V. Lyubimkin                             *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License                  *
+*   (version 3 or above) as published by the Free Software Foundation.    *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU GPL                        *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA               *
+**************************************************************************/
+#ifndef CUPT_PACKAGE_ID_SEEN
+#define CUPT_PACKAGE_ID_SEEN
+
+namespace cupt {
+
+class CUPT_API PackageId
+{
+	uint32_t __id;
+ public:
+	PackageId();
+	explicit PackageId(const char* start, const char* end);
+	explicit PackageId(const string&);
+	const string& name() const;
+	uint32_t rawId() const;
+
+	bool operator==(const PackageId& other) const { return __id == other.__id; }
+	bool operator<(const PackageId& other) const { return __id < other.__id; }
+	operator bool() const { return __id; }
+
+	static bool compareByName(PackageId left, PackageId right) { return left.name() < right.name(); }
+};
+
+}
+
+namespace std {
+
+template <>
+struct CUPT_API hash< cupt::PackageId >
+{
+	size_t operator()(cupt::PackageId packageId) const
+	{
+		return packageId.rawId();
+	}
+};
+
+}
+
+#endif
+
---
+++ cpp/lib/src/packageid.cpp
@@ -0,0 +1,140 @@
+/**************************************************************************
+*   Copyright (C) 2012 by Eugene V. Lyubimkin                             *
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License                  *
+*   (version 3 or above) as published by the Free Software Foundation.    *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU GPL                        *
+*   along with this program; if not, write to the                         *
+*   Free Software Foundation, Inc.,                                       *
+*   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA               *
+**************************************************************************/
+#include <cstddef>
+#include <unordered_map>
+
+#include <cupt/common.hpp>
+
+namespace cupt {
+
+namespace {
+
+class StringBuffer
+{
+	union
+	{
+		const char* __bufferStart;
+		const string* __string;
+	};
+	size_t __bufferLength;
+
+	bool __containsString() const { return __bufferLength; }
+
+ public:
+	StringBuffer(const char* start, size_t len)
+		: __bufferStart(start), __bufferLength(len)
+	{}
+	StringBuffer(const StringBuffer& other)
+		: __string(new string(other.__bufferStart, other.__bufferLength)), __bufferLength(0)
+	{}
+	StringBuffer(StringBuffer&& other)
+		: __bufferStart(other.__bufferStart), __bufferLength(other.__bufferLength)
+	{
+		other.__bufferLength = -1; // any non-zero value
+	}
+	~StringBuffer()
+	{
+		if (__containsString()) delete __string;
+	}
+	const char* getBufferStart() const
+	{
+		return __containsString() ? __string->data() : __bufferStart;
+	}
+	size_t getBufferLength() const
+	{
+		return __containsString() ? __string->size() : __bufferLength;
+	}
+	bool operator==(const StringBuffer& other) const
+	{
+		auto ourLength = getBufferLength();
+		if (ourLength != other.getBufferLength()) return false;
+		return memcmp(getBufferStart(), other.getBufferStart(), ourLength) == 0;
+	}
+	const string* getStringPtr() const
+	{
+		return __string;
+	}
+};
+struct StringBufferHasher
+{
+	size_t operator()(const StringBuffer& sb) const
+	{
+		auto buf = reinterpret_cast< const unsigned char* >(sb.getBufferStart());
+		auto end = buf + sb.getBufferLength();
+
+		size_t h = 0;
+		while (buf != end)
+		{
+			h = h*31 + *(buf++);
+		}
+
+		return h;
+	}
+};
+
+vector< const string* >& getN2S()
+{
+	static vector< const string* > n2s;
+	return n2s;
+}
+
+uint32_t getPackageNameId(StringBuffer&& packageName)
+{
+	typedef std::unordered_map< StringBuffer, uint32_t, StringBufferHasher > S2NType;
+
+	static S2NType s2n;
+	static uint32_t nextId = 1;
+
+	auto& id = s2n[std::move(packageName)];
+	if (!id)
+	{
+		id = nextId++;
+		auto persistentStringBuffer = (const StringBuffer*)
+				((const char*)(&id) - offsetof(S2NType::value_type, second));
+		getN2S().push_back(persistentStringBuffer->getStringPtr());
+	}
+
+	return id;
+}
+
+}
+
+PackageId::PackageId()
+	: __id(0)
+{}
+
+PackageId::PackageId(const char* start, const char* end)
+	: __id(getPackageNameId(StringBuffer(start, end-start)))
+{}
+
+PackageId::PackageId(const string& packageName)
+	: __id(getPackageNameId(StringBuffer(packageName.data(), packageName.size())))
+{}
+
+uint32_t PackageId::rawId() const
+{
+	return __id;
+}
+
+const string& PackageId::name() const
+{
+	return *(getN2S()[__id-1]);
+}
+
+}
+
