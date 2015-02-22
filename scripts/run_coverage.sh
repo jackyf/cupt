7a8d95e481d02f4f2c9392d2754ebe259570af58
Eugene V. Lyubimkin <jackyf@debian.org>
2015-02-22 17:51:47 +0200

    scripts: run_coverage: added

---
+++ scripts/run_coverage.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+OUTPUT=lcov.output.info
+
+set -e
+set -x
+
+ABSOLUTE_SOURCE_ROOT_PATH=`git rev-parse --show-toplevel`/cpp
+
+CAPTURE_OPTIONS="--directory . --base-directory "$ABSOLUTE_SOURCE_ROOT_PATH" --no-external"
+
+if [ -z "$ONLY_CAPTURE" ]; then
+	# recompile
+	CXXFLAGS=-coverage cmake ../..
+	make
+
+	# clean leftovers from previous tests
+	lcov --directory . --zerocounters
+
+	# gather all source files
+	lcov $CAPTURE_OPTIONS --capture --initial --output-file ${OUTPUT}.initial.base
+	lcov --remove ${OUTPUT}.initial.base '*CMake*Compiler*' --output-file ${OUTPUT}.initial
+
+	# test
+	make test
+fi
+
+# capture
+lcov $CAPTURE_OPTIONS --capture --output-file ${OUTPUT}.tests
+
+# combine
+lcov -a ${OUTPUT}.initial -a ${OUTPUT}.tests -o $OUTPUT
+
+# visualise
+genhtml --output-directory html --prefix $ABSOLUTE_SOURCE_ROOT_PATH $OUTPUT
+
