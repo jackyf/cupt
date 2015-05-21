c0d90e11f7b2afe77c649aee2c98a09609d897c1
Eugene V. Lyubimkin <jackyf@debian.org>
2015-05-21 21:37:35 +0300

    test: run: import TestCupt

--- test/run.sh
+++ test/run.sh
@@ -3,7 +3,7 @@
 set -e
 
 TESTS_PATH=$1
-RUNNER="perl -Mwarnings -Mstrict -I${TESTS_PATH}"
+RUNNER="perl -Mwarnings -Mstrict -I${TESTS_PATH} -MTestCupt"
 BINARY_UNDER_TEST_PATH=${PROVE_BINARY:-$2}
 prove --exec "$RUNNER" -r --timer $PROVE_PARAMS tt/$PROVE_FILTER :: "$BINARY_UNDER_TEST_PATH"
 
