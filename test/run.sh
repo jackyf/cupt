d08ca665586db1f2fbd52b701c1950d2a76bcf89
Eugene V. Lyubimkin <jackyf@debian.org>
2015-07-05 20:35:30 +0300

    test: run: make the tt/ symlink itself

--- test/run.sh
+++ test/run.sh
@@ -5,5 +5,7 @@ set -e
 TESTS_PATH=$1
 RUNNER="perl -Mwarnings -Mstrict -I${TESTS_PATH} -MTestCupt"
 BINARY_UNDER_TEST_PATH=${PROVE_BINARY:-$2}
+
+ln -sf $TESTS_PATH/t tt
 prove --exec "$RUNNER" -r --timer $PROVE_PARAMS tt/$PROVE_FILTER :: "$BINARY_UNDER_TEST_PATH"
 
