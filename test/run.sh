0caa19227ccae51bac655a6a85bc83de8adbd6c5
Eugene V. Lyubimkin <jackyf@debian.org>
2015-08-10 16:13:26 +0300

    test: run: do not create anything in 'tt' if it existed already
    Without '-T' ln(1) will put a new link to the 'tt' directory, instead of
    updating the 'tt' link itself.

--- test/run.sh
+++ test/run.sh
@@ -6,6 +6,6 @@ TESTS_PATH=$1
 RUNNER="perl -Mwarnings -Mstrict -I${TESTS_PATH} -MTestCupt"
 BINARY_UNDER_TEST_PATH=${PROVE_BINARY:-$2}
 
-ln -sf $TESTS_PATH/t tt
+ln -sTf $TESTS_PATH/t tt
 prove --exec "$RUNNER" -r --timer $PROVE_PARAMS tt/$PROVE_FILTER :: "$BINARY_UNDER_TEST_PATH"
 
